6ab074368d4370e16bed3e726833ea851e887a5b
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-01-12 20:39:42 -0300

    chore: sobe estrutura do projeto

---
+++ src/test/java/hebertesteves/tabelafipe/TabelafipeApplicationTests.java
@@ -0,0 +1,13 @@
+package hebertesteves.tabelafipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelafipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
