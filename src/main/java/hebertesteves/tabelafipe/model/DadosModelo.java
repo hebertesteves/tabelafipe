ad63dec551d5e0459c029fb76fa2c1c69132716c
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-01-12 21:37:35 -0300

    feat: adiciona record DadosModelo para agrupar modelos por marca

--- src/main/java/hebertesteves/tabelafipe/model/DadosModelo.java
+++ src/main/java/hebertesteves/tabelafipe/model/DadosModelo.java
@@ -1,4 +1,10 @@
 package hebertesteves.tabelafipe.model;
 
-public record DadosModelo() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosModelo(@JsonAlias("modelos") List<Modelo> modelos) {
 }
