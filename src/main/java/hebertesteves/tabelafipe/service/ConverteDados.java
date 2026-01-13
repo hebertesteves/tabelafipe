ffa5269069c99cc581cdc22b26b15e592c10c1d4
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-01-12 21:28:07 -0300

    feat: cria classe ConverteDados

--- src/main/java/hebertesteves/tabelafipe/service/ConverteDados.java
+++ src/main/java/hebertesteves/tabelafipe/service/ConverteDados.java
@@ -1,4 +1,19 @@
 package hebertesteves.tabelafipe.service;
 
-public class ConverteDados {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados {
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
