c1264a7496c046d2134bc3677519f68ec1219003
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-01-12 21:37:27 -0300

    feat: adiciona record DadosMarca para mapear retorno da API FIPE

--- src/main/java/hebertesteves/tabelafipe/model/DadosMarca.java
+++ src/main/java/hebertesteves/tabelafipe/model/DadosMarca.java
@@ -1,4 +1,9 @@
 package hebertesteves.tabelafipe.model;
 
-public record DadosMarca() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosMarca (@JsonAlias("codigo") Integer codigo,
+                         @JsonAlias("nome") String nome) {
 }
