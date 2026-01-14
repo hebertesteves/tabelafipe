61df8f70acb3d1dd428357c90ed722bcc718f9a4
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-01-13 21:14:50 -0300

    refac: melhora validacao de entrada e organizacao do fluxo principal

--- src/main/java/hebertesteves/tabelafipe/principal/Principal.java
+++ src/main/java/hebertesteves/tabelafipe/principal/Principal.java
@@ -4,87 +4,85 @@ import hebertesteves.tabelafipe.model.*;
 import hebertesteves.tabelafipe.service.ConsumoApi;
 import hebertesteves.tabelafipe.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Principal {
-    private ConsumoApi consumoApi = new ConsumoApi();
-    private Scanner sc = new Scanner(System.in);
-    private ConverteDados conversor = new ConverteDados();
+    private final ConsumoApi consumoApi = new ConsumoApi();
+    private final Scanner sc = new Scanner(System.in);
+    private final ConverteDados conversor = new ConverteDados();
 
-    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1";
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
     public void exibirMenu() {
         System.out.println("==== OPÇÕES ====");
-        System.out.println("Carros");
-        System.out.println("Motos");
-        System.out.println("Caminhões\n");
+        System.out.println("Carro");
+        System.out.println("Moto");
+        System.out.println("Caminhão\n");
 
         System.out.print("Digite uma das opções para consulta: ");
         String tipoVeiculo = sc.nextLine();
 
-        var json = consumoApi.consumirApi(URL_BASE + "/"
-                + tipoVeiculo.toLowerCase().trim()
-                +"/marcas");
+        String endereco = "";
+        if (tipoVeiculo.toLowerCase().contains("carr")) {
+            endereco = URL_BASE + "carros/marcas/";
+        } else if (tipoVeiculo.toLowerCase().trim().contains("mot")) {
+            endereco = URL_BASE + "motos/marcas/";
+        } else if (tipoVeiculo.toLowerCase().trim().contains("caminh")) {
+            endereco = URL_BASE + "caminhoes/marcas/";
+        } else {
+            System.out.println("ERRO! Veiculo não existe");
+            return;
+        }
+
+        var json = consumoApi.consumirApi(endereco);
 
         DadosMarca[] dadosMarca = conversor.obterDados(json, DadosMarca[].class);
+        List<DadosMarca> marcas = Arrays.stream(dadosMarca)
+                .sorted(Comparator.comparing(DadosMarca::codigo))
+                .toList();
 
-        for (DadosMarca dadoMarca : dadosMarca) {
-            System.out.println("Cód: " + dadoMarca.codigo()
-                    + "   Descrição: " + dadoMarca.nome());
-        }
+        marcas.forEach(marca ->
+                System.out.println("Cód: " + marca.codigo() + "   Descrição: " + marca.nome())
+        );
 
         System.out.print("\nInforme o código da marca para consulta: ");
         int codigoMarca = sc.nextInt();
         sc.nextLine();
 
-        json = consumoApi.consumirApi(URL_BASE + "/"
-                + tipoVeiculo.toLowerCase().trim()
-                + "/marcas/"
-                + codigoMarca
-                + "/modelos");
+        endereco = endereco + codigoMarca + "/modelos/";
+        json = consumoApi.consumirApi(endereco);
 
         DadosModelo dadosModelos = conversor.obterDados(json, DadosModelo.class);
+        List<Modelo> modelos = dadosModelos.modelos().stream()
+                .sorted(Comparator.comparing(Modelo::codigo))
+                .toList();
 
-        dadosModelos.modelos().forEach(modelo -> System.out.println("Cód: " + modelo.codigo()
-                + "   Descrição: " + modelo.nome()));
+        modelos.forEach(modelo ->
+                System.out.println("Cód: " + modelo.codigo() + "   Descrição: " + modelo.nome())
+        );
 
         System.out.print("\nDigite um trecho do nome do veículo para consulta: ");
         String trechoNomeVeiculo = sc.nextLine();
 
-        List<Modelo> modelosPorNome = dadosModelos.modelos().stream()
-                        .filter(m -> m.nome().toLowerCase().trim().contains(trechoNomeVeiculo))
-                                .collect(Collectors.toList());
+        List<Modelo> modelosFiltrados = modelos.stream()
+                        .filter(m -> m.nome().toLowerCase().contains(trechoNomeVeiculo.toLowerCase().trim()))
+                                .toList();
 
-        modelosPorNome.forEach(modelo -> System.out.println("Cód: " + modelo.codigo()
+        modelosFiltrados.forEach(modelo -> System.out.println("Cód: " + modelo.codigo()
                 + "   Descrição: " + modelo.nome()));
 
         System.out.print("\nDigite o código do modelo para consultar valores: ");
         int codigoModelo = sc.nextInt();
         sc.nextLine();
 
-        json = consumoApi.consumirApi(URL_BASE + "/"
-                + tipoVeiculo.toLowerCase().trim() +
-                "/marcas/"
-                + codigoMarca
-                + "/modelos/"
-                + codigoModelo
-                + "/anos");
+        endereco = endereco + codigoModelo + "/anos/";
+        json = consumoApi.consumirApi(endereco);
 
         DadosAno[] dadosAno = conversor.obterDados(json, DadosAno[].class);
 
         List<Veiculo> veiculos = new ArrayList<>();
         for (DadosAno ano : dadosAno) {
-            json = consumoApi.consumirApi(URL_BASE + "/"
-                    + tipoVeiculo.toLowerCase().trim()
-                    + "/marcas/"
-                    + codigoMarca
-                    + "/modelos/"
-                    + codigoModelo
-                    + "/anos/"
-                    + ano.codigo());
+            json = consumoApi.consumirApi(endereco + ano.codigo());
 
             DadosVeiculo dadosVeiculo = conversor.obterDados(json, DadosVeiculo.class);
             veiculos.add(new Veiculo(dadosVeiculo));
